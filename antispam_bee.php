e42deedd811c753cf2652ac786af964a119b3fc5
2ndkauboy <bernhard@kau-boys.de>
2017-01-18 23:51:39 +0100

    Implemented the regex replace callback function

--- antispam_bee.php
+++ antispam_bee.php
@@ -1229,21 +1229,11 @@ class Antispam_Bee {
 			return $data;
 		}
 
-		/* Build init time field */
-		if ( self::get_option('time_check') ) {
-			$init_time_field = sprintf(
-				'<input type="hidden" name="ab_init_time" value="%d" />',
-				time()
-			);
-		} else {
-			$init_time_field = '';
-		}
-
 		/* Inject HTML */
-		return preg_replace(
+		return preg_replace_callback(
 			'/(?P<all>                                                              (?# match the whole textarea tag )
 				<textarea                                                           (?# the opening of the textarea and some optional attributes )
-				(                                                                   (?# match a class attribute followed by some optional ones and the src attribute )
+				(                                                                   (?# match a id attribute followed by some optional ones and the name attribute )
 					(?P<before1>[^>]*)
 					(?P<id1>id=["\'](?P<id_value1>[^>"\']*)["\'])
 					(?P<between1>[^>]*)
@@ -1261,14 +1251,47 @@ class Antispam_Bee {
 				(?P<after>[^>]*)                                                    (?# match any additional optional attributes )
 				><\/textarea>                                                       (?# the closing of the textarea )
 			)/x',
-            sprintf(
-                '<textarea$1name="%s"$2</textarea><textarea name="comment" style="display:none" rows="1" cols="1"></textarea>%s',
-                self::$_secret,
-                $init_time_field
-            ),
+            array( 'Antispam_Bee', 'replace_comment_field_callback' ),
 			$data,
 			1
 		);
+	}
+
+	/**
+	 * The callback function for the preg_match_callback to modify the textarea tags.
+	 *
+	 * @since   2.6.10
+	 *
+	 * @param array $matches The regex matches.
+	 *
+	 * @return string The modified content string.
+	 */
+	public static function replace_comment_field_callback( $matches ) {
+		/* Build init time field */
+		if ( self::get_option( 'time_check' ) ) {
+			$init_time_field = sprintf(
+				'<input type="hidden" name="ab_init_time" value="%d" />',
+				time()
+			);
+		} else {
+			$init_time_field = '';
+		}
+
+		$output = '<textarea ' . $matches['before1'] . $matches['before2'] . $matches['before3'];
+
+		if ( ! empty( $matches['id1'] ) ) {
+			$output .= 'id="' . $matches['id_value1'] . '" ';
+		} elseif ( ! empty( $matches['id2'] ) ) {
+			$output .= 'id="' . $matches['id_value2'] . '" ';
+		}
+
+		$output .= ' name="' . self::$_secret . '" ';
+		$output .= $matches['between1'] . $matches['between2'] . $matches['between3'];
+		$output .= $matches['after'] . '>';
+		$output .= '</textarea>';
+		$output .= $init_time_field;
+
+		return $output;
 	}
 
 
