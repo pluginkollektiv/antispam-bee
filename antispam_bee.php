9e65f72f79f47be4323efccc075341789bc2892b
websupporter <webmaster@websupporter.net>
2018-06-01 10:21:36 +0300

    phpcs

--- antispam_bee.php
+++ antispam_bee.php
@@ -1242,23 +1242,21 @@ class Antispam_Bee {
 			);
 		}
 
-
 		if ( $options['regexp_check'] && self::_is_regexp_spam(
-				array(
-					'ip'     => $ip,
-					'rawurl' => $url,
-					'host'   => parse_url( $url, PHP_URL_HOST ),
-					'body'   => $body,
-					'email'  => '',
-					'author' => '',
-				)
-			) ) {
+			array(
+				'ip'     => $ip,
+				'rawurl' => $url,
+				'host'   => parse_url( $url, PHP_URL_HOST ),
+				'body'   => $body,
+				'email'  => '',
+				'author' => '',
+			)
+		) ) {
 			return array(
 				'reason' => 'regexp',
 			);
 		}
 	}
-
 
 	/**
 	 * Check the comment
