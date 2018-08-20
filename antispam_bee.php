4d472d9c755e631c6f639f19db17cb7132a65771
websupporter <webmaster@websupporter.net>
2018-08-20 16:16:46 +0300

    tag 2.9.0-beta

--- antispam_bee.php
+++ antispam_bee.php
@@ -9,7 +9,7 @@
  * Domain Path: /lang
  * License:     GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version:     2.8.1
+ * Version:     2.9.0-beta
  *
  * @package Antispam Bee
  **/
