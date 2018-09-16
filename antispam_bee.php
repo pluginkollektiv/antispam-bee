b4feb5acb1c9c51ffdd120e40b30f163d1d4afe3
David Remer <webmaster@websupporter.net>
2018-09-16 15:07:20 +0300

    make 2.9.0

--- antispam_bee.php
+++ antispam_bee.php
@@ -9,7 +9,7 @@
  * Domain Path: /lang
  * License:     GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version:     2.9.0-beta
+ * Version:     2.9.0
  *
  * @package Antispam Bee
  **/
