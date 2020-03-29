83be10f54d18f025beb9a9e108d928fa4172a3bd
Torsten Landsiedel <torsten.landsiedel@gmx.de>
2020-03-29 18:29:06 +0200

    Add comment user agent to regex pattern check (fixes #322)

--- antispam_bee.php
+++ antispam_bee.php
@@ -1351,11 +1351,12 @@ class Antispam_Bee {
 	 * @return  array|void     Array with suspected reason
 	 */
 	private static function _verify_comment_request( $comment ) {
-		$ip     = self::get_key( $comment, 'comment_author_IP' );
-		$url    = self::get_key( $comment, 'comment_author_url' );
-		$body   = self::get_key( $comment, 'comment_content' );
-		$email  = self::get_key( $comment, 'comment_author_email' );
-		$author = self::get_key( $comment, 'comment_author' );
+		$ip        = self::get_key( $comment, 'comment_author_IP' );
+		$url       = self::get_key( $comment, 'comment_author_url' );
+		$body      = self::get_key( $comment, 'comment_content' );
+		$email     = self::get_key( $comment, 'comment_author_email' );
+		$author    = self::get_key( $comment, 'comment_author' );
+		$useragent = self::get_key( $comment, 'comment_agent' );
 
 		if ( empty( $body ) ) {
 			return array(
@@ -1407,12 +1408,13 @@ class Antispam_Bee {
 
 		if ( $options['regexp_check'] && self::_is_regexp_spam(
 			array(
-				'ip'     => $ip,
-				'rawurl' => $url,
-				'host'   => self::parse_url( $url, 'host' ),
-				'body'   => $body,
-				'email'  => $email,
-				'author' => $author,
+				'ip'        => $ip,
+				'rawurl'    => $url,
+				'host'      => self::parse_url( $url, 'host' ),
+				'body'      => $body,
+				'email'     => $email,
+				'author'    => $author,
+				'useragent' => $useragent,
 			)
 		) ) {
 			return array(
@@ -1528,6 +1530,7 @@ class Antispam_Bee {
 			'body',
 			'email',
 			'author',
+			'useragent',
 		);
 
 		$patterns = array(
