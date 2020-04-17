396115b9880bc5d021b0c181be29b43f707317b7
David Remer <webmaster@websupporter.net>
2020-04-17 15:13:14 +0300

    remove -beta flag from version

--- antispam_bee.php
+++ antispam_bee.php
@@ -9,7 +9,7 @@
  * Domain Path: /lang
  * License:     GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version:     2.9.2-beta
+ * Version:     2.9.2
  *
  * [](http://coderisk.com/wp/plugin/antispam-bee/RIPS-lAHLcgvqY8)
  *
