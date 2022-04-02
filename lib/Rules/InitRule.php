e3e24d42126ad22c8ad2702bbb4bc0f774d4cc40
Bernhard Kau <bernhard@kau-boys.de>
2022-04-02 14:20:38 +0200

    replace the anonymous callback function adding the rule to make it easier to remove it later

--- lib/Rules/InitRule.php
+++ lib/Rules/InitRule.php
@@ -3,16 +3,28 @@
 namespace AntispamBee\Rules;
 
 trait InitRule {
-	public static function init() {
-		add_filter(
-			'asb_rules',
-			function ( $rules ) {
-				$rules[] = [
-					'verifiable' => self::class,
-				];
 
-				return $rules;
-			}
-		);
+	/**
+	 * Initialize the rule.
+	 *
+	 * @return void
+	 */
+	public static function init() {
+		add_filter( 'asb_rules', [ __CLASS__, 'add_rule' ] );
+	}
+
+	/**
+	 * Add the current rule class to the 'asb_rules' filter.
+	 *
+	 * @param array $rules The currently registered rules.
+	 *
+	 * @return array
+	 */
+	public static function add_rule( $rules ) {
+		$rules[] = [
+			'verifiable' => self::class,
+		];
+
+		return $rules;
 	}
 }
