800c2ab69d9194fd34fcdbb42f096faeb983c4a4
Florian Brinkmann <kontakt@florianbrinkmann.com>
2023-05-24 22:05:11 +0200

    remove unneeded UpdateSpamCount class

--- src/load.php
+++ src/load.php
@@ -25,7 +25,6 @@ use AntispamBee\PostProcessors\Delete;
 use AntispamBee\PostProcessors\DeleteForReasons;
 use AntispamBee\PostProcessors\SaveReason;
 use AntispamBee\PostProcessors\SendEmail;
-use AntispamBee\PostProcessors\UpdateSpamCount;
 use AntispamBee\PostProcessors\UpdateSpamLog;
 use AntispamBee\Rules\ApprovedEmail;
 use AntispamBee\Rules\BBCode;
@@ -64,7 +63,6 @@ function init() {
 		DeleteForReasons::class,
 		SaveReason::class,
 		SendEmail::class,
-		UpdateSpamCount::class,
 		UpdateSpamLog::class,
 		// Rules
 		ApprovedEmail::class,
