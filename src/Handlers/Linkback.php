1b21b365944d88060331a8dd2c27147727db9d7f
Florian Brinkmann <kontakt@florianbrinkmann.com>
2023-06-02 09:55:34 +0200

    fix: add checks for empty linkbacks, refs #500

--- src/Handlers/Linkback.php
+++ src/Handlers/Linkback.php
@@ -4,29 +4,38 @@ namespace AntispamBee\Handlers;
 
 use AntispamBee\GeneralOptions\IgnoreLinkbacks;
 use AntispamBee\Helpers\ContentTypeHelper;
+use AntispamBee\Helpers\IpHelper;
 
 class Linkback extends Reaction {
 	protected static $content_type = 'linkback';
 
-	public static function process( $reaction ) {
-		if ( ! ContentTypeHelper::reaction_is_one_of( $reaction, [ 'pingback', 'trackback', 'pings' ], '' ) ) {
-			return $reaction;
+	public static function process( $linkback ) {
+		if ( ! ContentTypeHelper::reaction_is_one_of( $linkback, [ 'pingback', 'trackback', 'pings' ], '' ) ) {
+			return $linkback;
 		}
 
 		if ( IgnoreLinkbacks::is_active() ) {
-			return $reaction;
+			return $linkback;
 		}
 
-		if ( self::is_linkback_title_blog_name( $reaction ) ) {
-			return self::handle_spam( $reaction, [ 'asb-title-is-blogname' ] );
+		if ( self::is_linkback_title_blog_name( $linkback ) ) {
+			return self::handle_spam( $linkback, [ 'asb-title-is-blogname' ] );
 		}
 
-		parent::process( $reaction );
+		$ip   = IpHelper::get_client_ip();
+		$url  = $linkback['comment_author_url'] ?? '';
+		$body = $linkback['comment_content'];
+
+		if ( empty( $url ) || empty( $body ) || empty( $ip ) ) {
+			return self::handle_spam( $linkback, [ 'asb-empty' ] );
+		}
+
+		parent::process( $linkback );
 	}
 
-	protected static function is_linkback_title_blog_name( $reaction ) {
-		$body = $reaction['comment_content'];
-		$blog_name = $reaction['comment_author'];
+	protected static function is_linkback_title_blog_name( $linkback ) {
+		$body = $linkback['comment_content'];
+		$blog_name = $linkback['comment_author'];
 
 		preg_match( '/<strong>(.*)<\/strong>\\n\\n/', $body, $matches );
 		if ( ! isset( $matches[1] ) ) {
