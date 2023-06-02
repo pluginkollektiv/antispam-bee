2492bea294833606b34377983f4226842642f923
Florian Brinkmann <kontakt@florianbrinkmann.com>
2023-06-02 10:02:07 +0200

    fix: wrong returned spam score from LinkbackFromMyself check

--- src/Rules/LinkbackFromMyself.php
+++ src/Rules/LinkbackFromMyself.php
@@ -24,31 +24,31 @@ class LinkbackFromMyself extends Base implements SpamReason {
 		$url            = $url[0];
 		$target_post_id = $target_post_id[0];
 		if ( 0 !== strpos( $url, home_url() ) ) {
-			return - 1;
+			return 0;
 		}
 
-		$original_post_id = (int) url_to_postid( $url );
+		$original_post_id = url_to_postid( $url );
 		if ( ! $original_post_id ) {
-			return - 1;
+			return 0;
 		}
 
 		$post = get_post( $original_post_id );
 		if ( ! $post ) {
-			return - 1;
+			return 0;
 		}
 
 		$urls        = wp_extract_urls( $post->post_content );
 		$url_to_find = get_permalink( $target_post_id );
 		if ( ! $url_to_find ) {
-			return - 1;
+			return 0;
 		}
 		foreach ( $urls as $url ) {
 			if ( strpos( $url, $url_to_find ) === 0 ) {
-				return 1;
+				return -10;
 			}
 		}
 
-		return - 1;
+		return 0;
 	}
 
 	public static function get_name() {
