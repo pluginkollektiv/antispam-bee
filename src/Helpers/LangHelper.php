749ab9e8c61c1f68fb3194831e25af6b98e0c94c
Bernhard Kau <github@kau-boys.de>
2026-07-03 19:05:39 +0200

    chore: update comments for spellchecker directives in LangHelper

--- src/Helpers/LangHelper.php
+++ src/Helpers/LangHelper.php
@@ -23,7 +23,7 @@ class LangHelper {
 	 * @since   2.9.0
 	 */
 	public static function map( string $franc_code ): string {
-		// spellchecker:off
+		// spellchecker:off.
 		$codes = [
 			'zha' => 'za',
 			'zho' => 'zh',
@@ -211,7 +211,7 @@ class LangHelper {
 			'aze' => 'az',
 			'nds' => 'de',
 		];
-		// spellchecker:on
+		// spellchecker:on.
 
 		if ( array_key_exists( $franc_code, $codes ) ) {
 			return $codes[ $franc_code ];
